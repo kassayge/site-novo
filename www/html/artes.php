5ae37459bf85b6b2c68dc7dd4e71de86e1de34ae
Kassayge <116615755+kassayge@users.noreply.github.com>
2023-01-14 20:26:45 -0300

    featured(#3)
    adicionado uma pasta propria para imagens alem do conserto da navbar ficar se mexendo

--- www/html/artes.php
+++ www/html/artes.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="_css/favicon.ico" type="image/ico"> <!-- muda o icone da pagina -->
-    <title>criado!!</title>
-    <link rel="stylesheet" href="_css/estilo.css"/>
-</head>
-<body class="artes">
-    <header>
-        <nav class=navbar>  <!-- barra de navegação usando tipo de lista-->
-            <ul class="ul">
-                <a href="index.php"><button class="a" href="#">inicio</button></a>
-                <a href="coisitas.php"><button class="a">coisitas</button></a>
-                <a href="artes.php"><button class="a">artes</button></a>
-                <a href="testes.php"><button class="a" >testes</button></a>
-                <a href="sobre.php"><button class="a">sobre mim</button></a>
-            </ul>
-        </nav>
-    </header>              
+<?php
+include "header.php";
+?>
+
+<body class="artes">             
     <div class="desenhos"> <!-- definindo para o que vai servir a pagina com o class -->
         <img class="dese" src="/desenhos/teste.png" alt="autoretrato no estilo chibi"> <!-- aqui tem a arte/imagem -->
         <p>foi um desenho recente feito na madrugada de 13 de jan, foi um desenho q tive inspiração por conta de um site asiatico chamado picrew</p>
--- www/html/sobre.php
+++ www/html/sobre.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="_css/favicon.ico" type="image/ico">
-    <title>informações do criador</title>
-    <link rel="stylesheet" href="_css/estilo.css"/>
-</head>
+<?php
+include "header.php";
+?>
+
 <body class="sobre">
-    <header>
-        <nav class=navbar>
-            <ul class="ul">
-                <a href="index.php"><button class="a">inicio</button></a>
-                <a href="coisitas.php"><button class="a">coisitas</button></a>
-                <a href="artes.php"><button class="a">artes</button></a>
-                <a href="testes.php"><button class="a" >testes</button></a>
-                <a href="sobre.php"><button class="a">sobre mim</button></a>
-            </ul>
-        </nav>
-     </header>
     <div class="contato">
         e-mail:gugafiebes@gmail.com
         whatzap:(47) 99160-8228 
