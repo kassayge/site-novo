66d2c5d0034b5a2810cf14a3a2e461c705da7cc1
kassayge <gugafiebes@gmail.com>
2023-01-14 20:25:26 -0300

    adicionado uma pasta propria para imagens alem do conserto da navbar ficar se mexendo

--- www/html/testes.php
+++ www/html/testes.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="_css/favicon.ico" type="image/ico">
-    <title>recriando...</title>
-    <link rel="stylesheet" href="_css/estilo.css"/>
-</head>
+<?php
+include "header.php";
+?>
 
 <body class="testes">
-    <header>
-        <nav class=navbar>
-            <ul class="ul">
-                <a href="index.php"><button class="a" href="#">inicio</button></a>
-                <a href="coisitas.php"><button class="a">coisitas</button></a>
-                <a href="artes.php"><button class="a">artes</button></a>
-                <a href="testes.php"><button class="a" >testes</button></a>
-                <a href="sobre.php"><button class="a">sobre mim</button></a>
-            </ul>
-        </nav>
-    </header>
     <div class="div">
         <p>ola mundo</p>
     </div>
